a6b046a2416adf32182e7050fe9a435184e7c0a0
YyUuJj <islamkaawad@gmail.com>
2022-03-09 22:51:30 +0600

    add form reg

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 import route
 
 
 app = Flask(__name__)
 
-menu = ["one", "two", "three"]
+menu = [
+    {"name": "Главная", "url": "index"},
+    {"name": 'О нас', "url": "about"},
+    {"name": "Регистрация", "url": "registration"}
+]
+
 
 num = 0
 
+users = []
+
 @app.route("/")
+@app.route("/index")
 def index():
     return render_template('index.html', title="test", menu=menu, num = num)
 
 
 @app.route("/about")
 def about():
-    return "<h1>Hello world</h1>"
+    return render_template('about.html', title="about", menu=menu)
+
+@app.route("/registration", methods=['POST',"GET"])
+def registration():
+    if request.method == "POST":
+        users.append(request.form['username'])
+        print(users)
+        print(request.form['username'])
+
+    return render_template('registration.html', title="registration", menu=menu, users=users)
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Имя пользователя: {username}"
+    print(url_for())
 
 
 if __name__ == "__main__":
