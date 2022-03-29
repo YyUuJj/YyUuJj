4b18cd43b425780761852235e312f0b3c9f04473
YyUuJj <islamkaawad@gmail.com>
2022-03-29 21:44:31 +0600

    'addListUsers'

--- admin/admin.py
+++ admin/admin.py
@@ -1,12 +1,27 @@
+import sqlite3
+from flask import Blueprint, g, redirect, url_for, render_template, request, flash, session
 
-import re
-from turtle import title
-from flask import Blueprint, redirect, url_for, render_template, request, flash, session
+
 
 menu = [{'url': ".index", 'title': 'Панель'},
+        {'url': ".listpubs", 'title': 'Список статей'},
+        {'url': ".listusers", 'title': 'Список пользователей'},
         {'url': '.logout', 'title': 'Выйти'}]
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+
+db = None
+
+@admin.before_request
+def before_request():
+    global db
+    db = g.get('link_db')
+
+@admin.teardown_request
+def teardown_request(request):
+    global db
+    db = None
+    return request
 
 def login_admin():
     session['admin_logged'] = 1
@@ -42,3 +57,36 @@ def logout():
     logout_admin()
 
     return redirect(url_for('.login'))
+
+
+@admin.route('/list-pubs')
+def listpubs():
+    if not isLogged():
+        return redirect(url_for('.login'))
+    
+    list = []
+    if db:
+        try:
+            cur = db.cursor()
+            cur.execute(f'SELECT title, text, url FROM posts')
+            list = cur.fetchall()
+        except sqlite3.Error as e:
+            print("Ошибка получения статей из БД" + str(e))
+        
+        return render_template('admin/listpubs.html', title="Список статей", menu=menu, list=list)
+
+@admin.route('/list-users')
+def listusers():
+    if not isLogged():
+        return redirect(url_for('.login'))
+    
+    list = []
+    if db:
+        try:
+            cur = db.cursor()
+            cur.execute(f'SELECT name, email FROM users ORDER BY time DESC')
+            list = cur.fetchall()
+        except sqlite3.Error as e:
+            print("Ошибка получения статей из БД" + str(e))
+        
+        return render_template('admin/listusers.html', title="Список пользователей", menu=menu, list=list)
